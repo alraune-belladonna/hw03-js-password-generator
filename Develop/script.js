aed93df196ab25629d2e339258aa0ccc0c518719
Albedo <raincadence27@gmail.com>
2022-02-20 19:38:52 -0800

    attempt working password

--- Develop/script.js
+++ Develop/script.js
@@ -3,31 +3,29 @@ const inputs = document.querySelector('.qualities')
 //arrays
 
 const alphabetList = 'abcdefghijklmnopqrstuvwxyz'
-
 const upperAlphabetList = alphabetList.toUpperCase()
-
 const numberList = '0123456789'
-
 const specialList = '!#$%&()*+,-./:;<=>?@[]/^_`{|}~'
 
 let lowercaseArray = alphabetList.split(' ')
-
 let uppercaseArray = upperAlphabetList.split(' ')
-
 let numericArray = numberList.split(' ')
-
 let specialArray = specialList.split(' ')
 
 //number of characters quality
 
-let numberOfCharacters = document.getElementById('numberChar').value
+document.getElementById('numberBtn').addEventListener('click', () => {
+  let numberOfCharacters = document.getElementById('numberChar').value
+
+  localStorage.setItem('numberOfCharacters', numberOfCharacters)
+})
+
+//section - button active toggle
 
 inputs.addEventListener('click', () => {
   const element = event.target
   let state = element.getAttribute('data-state')
   //console.log('click works')
-
-//section - button active toggle
 
   if (element.matches('.toggle')) {
     if (state === 'off') {
@@ -45,55 +43,60 @@ inputs.addEventListener('click', () => {
   //password attribute toggles
 
   if (document.getElementById('lowercase').getAttribute('data-state') === 'off') {
-    let lowercaseArray = []
+    let array1 = []
     console.log('lowercase off')
   }
 
   else if (document.getElementById('lowercase').getAttribute('data-state') === 'on') {
+    let array1 = lowercaseArray
     console.log('lowercase on')
   }
 
   if (document.getElementById('uppercase').getAttribute('data-state') === 'off') {
-    let uppercaseArray = []
+    let array2 = []
     console.log('uppercase off')
   }
 
   else if (document.getElementById('uppercase').getAttribute('data-state') === 'on') {
+    let array2 = uppercaseArray
     console.log('uppercase on')
   }
 
   if (document.getElementById('numeric').getAttribute('data-state') === 'off') {
-    let numericArray = []
+    let array3 = []
     console.log('numeric off')
   }
 
   else if (document.getElementById('numeric').getAttribute('data-state') === 'on') {
+    let array3 = numericArray
     console.log('numeric on')
   }
 
   if (document.getElementById('special').getAttribute('data-state') === 'off') {
-    let specialArray = []
+    let array4 = []
     console.log('special off')
   }
 
   else if (document.getElementById('special').getAttribute('data-state') === 'on') {
+    let array4 = specialArray
     console.log('special on')
   }
 
-  let characterArray = [lowercaseArray.concat(uppercaseArray, numericArray, specialArray)]
+  let arrayData = [array1.concat(array2, array3, array4)]
 
-  localStorage.setItem('arrayData', characterArray)
-
+  console.log(arrayData)
 })
 
 //final array set connect to inside generate function
 let generatePassword = () => {
 
-  let password = localStorage.getItem('arrayData')[Math.floor(Math.random() * numberOfCharacters)]
 
-  document.getElementById('password').placeholder = password
 }
 
 document.getElementById('generate').addEventListener('click', () => {
   generatePassword()
+
+  let password = arrayData[Math.floor(Math.random() * arrayData.length)] 
+
+  document.getElementById('password').placeholder = password
 })
