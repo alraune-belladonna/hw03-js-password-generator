0edc1929561e5f2d635811ac7cc83d88ba1c2f6a
Albedo <raincadence27@gmail.com>
2022-02-22 00:37:40 -0800

    complete in theory, not working

--- Develop/script.js
+++ Develop/script.js
@@ -2,101 +2,252 @@ const inputs = document.querySelector('.qualities')
 
 //arrays
 
-const alphabetList = 'abcdefghijklmnopqrstuvwxyz'
-const upperAlphabetList = alphabetList.toUpperCase()
-const numberList = '0123456789'
-const specialList = '!#$%&()*+,-./:;<=>?@[]/^_`{|}~'
+let lowercaseArray = 'abcdefghijklmnopqrstuvwxyz'
+let uppercaseArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let numericArray = '0123456789'
+let specialArray = '!#$%&()*+,-./:;<=>?@[]^_`{|}~'
 
-let lowercaseArray = alphabetList.split(' ')
-let uppercaseArray = upperAlphabetList.split(' ')
-let numericArray = numberList.split(' ')
-let specialArray = specialList.split(' ')
+let fullArray = ''
+let lengthValue
 
-//number of characters quality
+//password options
 
-document.getElementById('numberBtn').addEventListener('click', () => {
-  let numberOfCharacters = document.getElementById('numberChar').value
+let lowercaseConfirm
+let uppercaseConfirm
+let numericConfirm
+let specialConfirm
 
-  localStorage.setItem('numberOfCharacters', numberOfCharacters)
-})
+const passwordGenerator = () => {
 
-//section - button active toggle
+  let lowercaseConfirm = confirm('Include lowercase letters in password?')
+  let uppercaseConfirm = confirm('Include uppercase letters in password?')
+  let numericConfirm = confirm('Include numeric values in password?')
+  let specialConfirm = confirm('Include special characters in password?')
+  let lengthConfirm = prompt('How long would you like your password to be?')
 
-inputs.addEventListener('click', () => {
-  const element = event.target
-  let state = element.getAttribute('data-state')
-  //console.log('click works')
+  if (isNaN (lengthConfirm) || (lengthConfirm <= 8 || lengthConfirm >= 128) == true ) {
+    alert('Please enter a numerical value for the password length between 8 and 128')
+  }
+  else {
+    lengthValue = parseInt(lengthConfirm)
+  }
 
-  if (element.matches('.toggle')) {
-    if (state === 'off') {
-      element.classList.add('active')
-      element.setAttribute('data-state', 'on')
-      //console.log('toggle on works')
+  if (lowercaseConfirm === true) {
+    fullArray += lowercaseArray
+  }
+
+  if (uppercaseConfirm === true) {
+    fullArray += uppercaseArray
+  }
+
+  if (numericConfirm === true) {
+    fullArray += numericArray
+  }
+
+  if (specialConfirm === true) {
+    fullArray += specialArray
+  }
+
+  for (let i = 0; i < lengthValue; i++) {
+    let randomIndex = Math.floor(Math.random() * fullArray.length)
+    let randomCharacter = fullArray[randomIndex]
+    password = password + randomCharacter
+
+    return password
+    return lowercaseConfirm
+    return uppercaseConfirm
+    return numericConfirm
+    return specialConfirm
+
+  }
+
+}
+
+let protoPassword
+const passwordCheck = () => {
+  protoPassword = passwordGenerator()
+  if (lowercaseConfirm) {
+    console.log('confirming lowercase')
+    let verifyLowercase = false
+    for (let i = 0; i < protoPassword.length; i++) {
+      let possibleLowercase = protoPassword[i]
+      if (possibleLowercase == 'a' ||  possibleLowercase == 'b' ||  possibleLowercase == 'c' ||  possibleLowercase == 'd' ||  possibleLowercase == 'e' ||  possibleLowercase == 'f' ||  possibleLowercase == 'g' ||  possibleLowercase == 'h' ||  possibleLowercase == 'i' ||  possibleLowercase == 'j' ||  possibleLowercase == 'k' ||  possibleLowercase == 'l' ||  possibleLowercase == 'm' ||  possibleLowercase == 'n' ||  possibleLowercase == 'o' ||  possibleLowercase == 'p' ||  possibleLowercase == 'q' ||  possibleLowercase == 'r' ||  possibleLowercase == 's' ||  possibleLowercase == 't' ||  possibleLowercase == 'u' ||  possibleLowercase == 'v' ||  possibleLowercase == 'w' ||  possibleLowercase == 'x' ||  possibleLowercase == 'y' ||  possibleLowercase == 'z') {
+        verifyLowercase = true
+      }
     }
-    else if (state === 'on') {
-      element.classList.remove('active')
-      element.setAttribute('data-state', 'off')
-      //console.log('toggle off works')
+    if (verifyLowercase == false) {
+      console.log('no lowercase')
+      passwordCheck()
     }
   }
 
-  //password attribute toggles
-
-  if (document.getElementById('lowercase').getAttribute('data-state') === 'off') {
-    let array1 = []
-    console.log('lowercase off')
+  if (uppercaseConfirm) {
+    let verifyUppercase = false
+    for (let i = 0; i < protoPassword.length; i++) {
+      let possibleUppercase = protoPassword[i]
+      if (possibleUppercase == 'A' || possibleUppercase == 'B' ||  possibleUppercase == 'C' ||  possibleUppercase == 'D' ||  possibleUppercase == 'E' ||  possibleUppercase == 'F' ||  possibleUppercase == 'G' ||  possibleUppercase == 'H' ||  possibleUppercase == 'I' ||  possibleUppercase == 'J' ||  possibleUppercase == 'K' ||  possibleUppercase == 'L' ||  possibleUppercase == 'M' ||  possibleUppercase == 'N' ||  possibleUppercase == 'O' ||  possibleUppercase == 'P' ||  possibleUppercase == 'Q' ||  possibleUppercase == 'R' ||  possibleUppercase == 'S' ||  possibleUppercase == 'T' ||  possibleUppercase == 'U' ||  possibleUppercase == 'V' ||  possibleUppercase == 'W' ||  possibleUppercase == 'X' ||  possibleUppercase == 'Y' ||  possibleUppercase == 'Z') {
+        verifyUppercase = true
+      }
+      if (verifyUppercase == false) {
+        console.log('no uppercase')
+        passwordCheck()
+      }
+    }
   }
 
-  else if (document.getElementById('lowercase').getAttribute('data-state') === 'on') {
-    let array1 = lowercaseArray
-    console.log('lowercase on')
+  if (numericConfirm) {
+    let verifyNumeric = false
+    for (let i = 0; i < protoPassword.length; i++) {
+      let possibleNumeric = protoPassword[i]
+      if (possibleNumeric == 0 || possibleNumeric == 1 || possibleNumeric == 2 || possibleNumeric == 3 || possibleNumeric == 4 || possibleNumeric == 5 || possibleNumeric == 6 || possibleNumeric == 7 || possibleNumeric == 8 || possibleNumeric == 9) {
+        verifyNumeric = true
+      }
+    }
+    if (verifyNumeric == false) {
+      console.log('no numeric')
+      passwordCheck()
+    }
   }
 
-  if (document.getElementById('uppercase').getAttribute('data-state') === 'off') {
-    let array2 = []
-    console.log('uppercase off')
+  if (specialConfirm) {
+    let verifySpecial = false
+    for (let i = 0; i < protoPassword.length; i++) {
+      let possibleSpecial = protoPassword[i]
+      if (possibleSpecial == '!' || possibleSpecial == '#' || possibleSpecial == '$' || possibleSpecial == '%' || possibleSpecial == '&' || possibleSpecial == '(' || possibleSpecial == ')' || possibleSpecial == '*' || possibleSpecial == '+' || possibleSpecial == ',' || possibleSpecial == '-' || possibleSpecial == '.' ||  possibleSpecial == '/' ||  possibleSpecial == ':' ||  possibleSpecial == ';' ||  possibleSpecial == '<' ||  possibleSpecial == '=' ||  possibleSpecial == '>' ||  possibleSpecial == '?' || possibleSpecial == '@' || possibleSpecial == '[' || possibleSpecial == ']' || possibleSpecial == '^' || possibleSpecial == '_' || possibleSpecial == '`' || possibleSpecial == '{' || possibleSpecial == '|' || possibleSpecial == '}' || possibleSpecial == '~') {
+        verifySpecial = true
+      }
+    }
+    if (verifySpecial == false) {
+      console.log('no special')
+      passwordCheck()
+    }
   }
 
-  else if (document.getElementById('uppercase').getAttribute('data-state') === 'on') {
-    let array2 = uppercaseArray
-    console.log('uppercase on')
-  }
-
-  if (document.getElementById('numeric').getAttribute('data-state') === 'off') {
-    let array3 = []
-    console.log('numeric off')
-  }
-
-  else if (document.getElementById('numeric').getAttribute('data-state') === 'on') {
-    let array3 = numericArray
-    console.log('numeric on')
-  }
-
-  if (document.getElementById('special').getAttribute('data-state') === 'off') {
-    let array4 = []
-    console.log('special off')
-  }
-
-  else if (document.getElementById('special').getAttribute('data-state') === 'on') {
-    let array4 = specialArray
-    console.log('special on')
-  }
-
-  let arrayData = [array1.concat(array2, array3, array4)]
-
-  console.log(arrayData)
-})
-
-//final array set connect to inside generate function
-let generatePassword = () => {
-
+  return protoPassword.value
 
 }
 
 document.getElementById('generate').addEventListener('click', () => {
-  generatePassword()
-
-  let password = arrayData[Math.floor(Math.random() * arrayData.length)] 
-
-  document.getElementById('password').placeholder = password
+  let newPassword = passwordCheck()
+  console.log(newPassword)
+  document.getElementById('password').innerHTML = newPassword
 })
+
+//discard - attempted nesting for loops to compare each individual value for checks
+
+// let protoPassword
+// const passwordCheck = () => {
+//   protoPassword = passwordGenerator()
+//   if(lowercaseConfirm) {
+//     let verifyLowercase = false
+//     for (let i = 0; i < lengthValue.length; i++) {
+//       let possibleLowercase = protoPassword[i];}
+//       for (let j = 0; j < lowercaseArray.length; j++) {
+//         eachLowercase = lowercaseArray[j];})
+
+//       if (eachLowercase = possibleLowercase) {
+//         let verifyLowercase = true
+//         alert(protoPassword)
+//       }
+
+//       else {
+//         passwordCheck()
+//       }
+//   }
+// }
+
+//discard - generate password
+
+// document.getElementById('generate').addEventListener('click', () => {
+//   compileArray()
+
+//   let password = localStorage.getItem('arrayData')[Math.floor(Math.random() * arrayData.length)]
+
+// discard - number of characters quality, may come back later
+
+// document.getElementById('numberBtn').addEventListener('click', () => {
+//   let numberOfCharacters = document.getElementById('numberChar').value
+
+//   localStorage.setItem('numberOfCharacters', numberOfCharacters)
+// })
+
+//discard - final array set connect to inside generate function
+
+// let compileArray = () => {
+//   let arrayData = [lowercaseArray.concat(uppercaseArray, numericArray, specialArray)]
+//   console.log(arrayData)
+
+//   localStorage.setItem('arrayData', arrayData)
+// }
+
+//discard - toggle buttons for password qualities, may come back later
+
+// inputs.addEventListener('click', () => {
+//   const element = event.target
+//   let state = element.getAttribute('data-state')
+//   //console.log('click works')
+
+//   if (element.matches('.toggle')) {
+//     if (state === 'off') {
+//       element.classList.add('active')
+//       element.setAttribute('data-state', 'on')
+//       //console.log('toggle on works')
+//     }
+//     else if (state === 'on') {
+//       element.classList.remove('active')
+//       element.setAttribute('data-state', 'off')
+//       //console.log('toggle off works')
+//     }
+//   }
+
+//   //password attribute toggles
+
+//   if (document.getElementById('lowercase').getAttribute('data-state') === 'off') {
+//     let lowercaseArray = []
+//     compileArray()
+//     console.log('lowercase off')
+//   }
+
+//   else if (document.getElementById('lowercase').getAttribute('data-state') === 'on') {
+//     let lowercaseArray = alphabetList.split(' ')
+//     compileArray()
+//     console.log('lowercase on')
+//   }
+
+//   if (document.getElementById('uppercase').getAttribute('data-state') === 'off') {
+//     let uppercaseArray = []
+//     compileArray()
+//     console.log('uppercase off')
+//   }
+
+//   else if (document.getElementById('uppercase').getAttribute('data-state') === 'on') {
+//     let uppercaseArray = upperAlphabetList.split(' ')
+//     compileArray()
+//     console.log('uppercase on')
+//   }
+
+//   if (document.getElementById('numeric').getAttribute('data-state') === 'off') {
+//     let numericArray = []
+//     compileArray()
+//     console.log('numeric off')
+//   }
+
+//   else if (document.getElementById('numeric').getAttribute('data-state') === 'on') {
+//     let numericArray = numberList.split(' ')
+//     compileArray()
+//     console.log('numeric on')
+//   }
+
+//   if (document.getElementById('special').getAttribute('data-state') === 'off') {
+//     let specialArray = []
+//     compileArray()
+//     console.log('special off')
+//   }
+
+//   else if (document.getElementById('special').getAttribute('data-state') === 'on') {
+//     let specialArray = specialList.split(' ')
+//     compileArray()
+//     console.log('special on')
+//   }
+
+// })
